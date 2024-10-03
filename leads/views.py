99d7ec322ed741922d6ed5897591edb3fc36dd0f
Harsh Khantwal <harshkhantwal786@gmail.com>
2024-10-03 16:21:48 +0530

    Mailgun commit

--- leads/views.py
+++ leads/views.py
@@ -75,13 +75,14 @@ class LeadCreateView(OrganisorAndLoginRequiredMixin, generic.CreateView):
         lead = form.save(commit=False)
         lead.organisation = self.request.user.userprofile
         lead.save()
-        # send_mail(
-        #     subject = "You've been assigned to the new lead.",
-        #     message = "Visit the site to know more",
-        #     from_email =settings.DEFAULT_FROM_EMAIL,
-        #     recipient_list = [lead.agent.user.email]
-        # )
-        return super(LeadCreateView, self).form_valid(form)                                                         
+        if lead.agent and lead.agent.user and lead.agent.user.email:
+            send_mail(
+                subject = "You've been assigned to the new lead.",
+                message = "Visit the site to know more",
+                from_email =settings.DEFAULT_FROM_EMAIL,
+                recipient_list = [lead.agent.user.email]
+            )
+            return super(LeadCreateView, self).form_valid(form)                                                         
 
 class LeadUpdateView(OrganisorAndLoginRequiredMixin, generic.UpdateView):
     template_name = 'leads/lead_update.html'
