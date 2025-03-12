835dc75d3614f42b120318ed1ae1e006468738db
Harsh <harshkhantwaal786@gmail.com>
2025-03-12 17:58:33 +0530

    fixed email sending

--- agents/views.py
+++ agents/views.py
@@ -1,4 +1,5 @@
-from django.core.mail import send_mail, SMTPException
+from django.core.mail import send_mail
+from smtplib import SMTPException
 from django.shortcuts import render, reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
