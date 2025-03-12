f76ce032246e03b52b8f0b4dddcb3d0fbaa992ba
Harsh <harshkhantwaal786@gmail.com>
2025-03-12 17:40:54 +0530

    fixed email sending

--- crm/settings.py
+++ crm/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv_path = BASE_DIR / ".env"
 load_dotenv(dotenv_path=dotenv_path)
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = True
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
